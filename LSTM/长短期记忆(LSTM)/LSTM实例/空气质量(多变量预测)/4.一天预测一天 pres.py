351e0ffa8409b5d1aee3250469d07e0cb4c7b144
世涛 张 <nowhitegive@foxmail.com>
2022-06-21 00:02:09 +0800

    press

--- LSTM/长短期记忆(LSTM)/LSTM实例/空气质量(多变量预测)/4.一天预测一天 pres.py
+++ LSTM/长短期记忆(LSTM)/LSTM实例/空气质量(多变量预测)/4.一天预测一天 pres.py
@@ -106,7 +106,7 @@ model.add(LSTM(300, input_shape=(train_X.shape[1], train_X.shape[2])))
 model.add(Dense(1))
 model.compile(loss='mae', optimizer='adam')
 # 拟合网络
-history = model.fit(train_X, train_y, epochs=10, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
+history = model.fit(train_X, train_y, epochs=2, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
 # 图像展示训练损失
 pyplot.plot(history.history['loss'], label='train')
 pyplot.plot(history.history['val_loss'], label='test')
